3ae60533ce932e0bc03ffcfeb0307901dfd56615
Jonathan Owen <jonathanjowen@gmail.com>
2020-04-04 09:29:40 -0600

    Initial commit.  plus_minus,py

---
+++ plus_minus.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the plusMinus function below.
+def plusMinus(arr):
+    arr_n   = len(arr)
+    arr_pos = len([i for i in arr if i > 0]) / arr_n
+    arr_neg = len([i for i in arr if i < 0]) / arr_n
+    print(arr_pos)
+    print(arr_neg)
+    print(1 - arr_pos - arr_neg)
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
