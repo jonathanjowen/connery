ed352bda7a1be75038a1b1e23a750d961ca38b39
Jonathan Owen <jonathanjowen@gmail.com>
2020-05-02 09:39:29 -0600

    kangaroo.py added

---
+++ kangaroo.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the kangaroo function below.
+def kangaroo(x1, v1, x2, v2):
+    x_gap = x2 - x1
+    v_rel = v1 - v2
+    if v_rel <= 0:
+        if x_gap == 0:
+            return "YES"
+        else:
+            return "NO"
+    elif x_gap%v_rel == 0:
+        return "YES"
+    else:
+        return "NO"
+
+y = product(a, b)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    x1V1X2V2 = input().split()
+
+    x1 = int(x1V1X2V2[0])
+
+    v1 = int(x1V1X2V2[1])
+
+    x2 = int(x1V1X2V2[2])
+
+    v2 = int(x1V1X2V2[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+
+
+# https://www.hackerrank.com/challenges/kangaroo/problem
