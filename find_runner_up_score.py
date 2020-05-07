faf5e9a44ddf271f5d90ab47f941cae59e9b2e93
Jonathan Owen <jonathanjowen@gmail.com>
2020-05-07 07:25:43 -0600

    find_runner_up_score.py added

---
+++ find_runner_up_score.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+    scores = list(arr)
+    win_score = max(scores)
+    winners = scores.count(win_score)
+    sorted_scores = sorted(scores, reverse=True)
+    print(sorted_scores[winners])
+
+# https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/
