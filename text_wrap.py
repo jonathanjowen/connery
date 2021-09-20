2aa4c9c5d9162ce3765dc553715efb7eb2d1237b
Jonathan Owen <jonathanjowen@gmail.com>
2021-09-20 13:35:55 -0600

    added text_wrap

---
+++ text_wrap.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import textwrap
+
+def wrap(string, max_width):
+    paragraph = [string[i : i + max_width] for i in range(0, len(string), max_width)]
+    return '\n'.join(paragraph)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
