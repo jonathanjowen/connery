aa7ce7478c0b05f6dce929fe88f6d864f7500268
Jonathan Owen <jonathanjowen@gmail.com>
2025-04-07 20:40:33 -0600

    updated triangle_quest_3

--- triangle_quest_3.py
+++ triangle_quest_3.py
@@ -1,9 +1,8 @@
 #!/bin/python3
 
 # Note: acceptable solutions needed to use only 2 lines of code, 1 for loop, no string functions
-#for i in range(1,int(input())): 
-#    print(i * sum([10**0, 10**1, 10**2, 10**3, 10**4, 10**5, 10**6, 10**7, 10**8][0:i])) 
+for i in range(1,int(input())): 
+    print(i * sum([10**0, 10**1, 10**2, 10**3, 10**4, 10**5, 10**6, 10**7, 10**8][0:i])) 
+# Better: print(i*10**i//9)
 
-
-print(10**range(1,9))
 # https://www.hackerrank.com/challenges/python-quest-1/problem
