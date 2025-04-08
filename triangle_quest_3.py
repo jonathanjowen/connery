f3c26a51b5eee78214728119ff153a7a44b1357b
Jonathan Owen <jonathanjowen@gmail.com>
2025-04-07 20:31:06 -0600

    added triangle_quest_3

---
+++ triangle_quest_3.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+# Note: acceptable solutions needed to use only 2 lines of code, 1 for loop, no string functions
+#for i in range(1,int(input())): 
+#    print(i * sum([10**0, 10**1, 10**2, 10**3, 10**4, 10**5, 10**6, 10**7, 10**8][0:i])) 
+
+
+print(10**range(1,9))
+# https://www.hackerrank.com/challenges/python-quest-1/problem
