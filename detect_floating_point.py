658325685615b583b865c02c262f27cae7fc3894
Jonathan Owen <jonathanjowen@gmail.com>
2025-04-14 11:24:11 -0600

    updated detect_floating_point

--- detect_floating_point.py
+++ detect_floating_point.py
@@ -1,22 +1,27 @@
 #!/bin/python3
 
+
 import re
 
 def read_integer():
     """Return an integer read from STDIN."""
     return(int(input()))
+
+def match_floating_point(string):
+    """Returns a match object if string is a floating point number or None otherwise"""       
+    floating_point_pattern = r'^[\+\-]?[0-9]*\.[0-9]+$'
+    return re.match(floating_point_pattern, test_string)
+
     
 if __name__ == '__main__':
     
     # n_of_tests = read_integer()
     n_of_tests = 1
-    
-    floating_point_pattern = r'^[\+\-]?[0-9]*\.[0-9]+$'
-    
+
     for _ in range(n_of_tests):
         # test_string = input()
-        test_string = '+ab.000'
-        print(re.match(floating_point_pattern, test_string) is not None)
+        test_string = '+123.000'
+        print(match_floating_point(test_string) is not None)
 
 
 # https://www.hackerrank.com/challenges/introduction-to-regex/problem
