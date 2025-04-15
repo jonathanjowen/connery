52c963f5ce8fceb76083fa04b60fe7963a7326dd
Jonathan Owen <jonathanjowen@gmail.com>
2025-04-14 23:01:26 -0600

    updated re_group_groups

--- re_group_groups.py
+++ re_group_groups.py
@@ -4,9 +4,11 @@
 import re
 
 if __name__ == '__main__':
-    s = input()
-    alphanumeric_only = re.sub(r'[^a-zA-Z0-9]', '', s)
-    m = re.search(r'(\w)\1', alphanumeric_only)
+    # s = input()
+    s = '__commit__'
+    # alphanumeric_only = re.sub(r'[^a-zA-Z0-9]', '', s)
+    m = re.search(r'([a-zA-Z0-9])\1', s)
+    # m = re.search(r'(\w)\1', alphanumeric_only)
     if m is not None:
         print(m.group(1))
     else:
