5b5497455c074ab6108ab36e65074a01eba137e7
Jonathan Owen <jonathanjowen@gmail.com>
2025-04-28 12:59:33 -0600

    added np_arrays

---
+++ np_arrays.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import numpy
+
+def arrays(arr):
+    return numpy.array(arr[::-1], float)
+
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
+
+# https://www.hackerrank.com/challenges/np-arrays/problem
