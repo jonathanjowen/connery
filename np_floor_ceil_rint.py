ff7f4c0cb7029a2dbb3164c028caff2c0a0edb9a
Jonathan Owen <jonathanjowen@gmail.com>
2025-04-29 23:44:03 -0600

    updated np_floor_ceil_rint

--- np_floor_ceil_rint.py
+++ np_floor_ceil_rint.py
@@ -4,16 +4,16 @@ import numpy
 numpy.set_printoptions(legacy='1.13')
 
 
-def read_float(sep=None):
-    """Return an iterator of integers from a STDIN line."""  
+def read_floats(sep=None):
+    """Return an iterator of floats from a STDIN line."""  
     return(map(float, input().split(sep)))
     
 if __name__ == '__main__':
-    a = numpy.array(list(read_float()))
+    a = numpy.array(list(read_floats()))
     print(numpy.floor(a))
     print(numpy.ceil(a))
     print(numpy.rint(a))
 
-    
+
 
 # https://www.hackerrank.com/challenges/floor-ceil-and-rint/problem
