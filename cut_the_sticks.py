dfa8ca868e2a5b32ccd98d3893f7a0a6abcc0166
Jonathan Owen <jonathanjowen@gmail.com>
2025-11-07 11:47:32 -0700

    added cut_the_sticks

---
+++ cut_the_sticks.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def cutTheSticks(arr):
+    n_of_sticks = []
+    while len(arr) > 0:
+        n_of_sticks.append(len(arr))
+        shortest_stick = min(arr)
+        arr = [stick - shortest_stick for stick in arr if stick > shortest_stick]
+    return n_of_sticks
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    result = cutTheSticks(arr)
+    print(result)
+
+# Note
+# SUbtraction of shortest_stick is consistent with description of
+# the problem but not necessary. If lengths aren't needed, the 
+# following definition of arr is sufficient
+# arr = [stick for stick in arr if stick > shortest_stick]
+
+# https://www.hackerrank.com/challenges/cut-the-sticks/problem
